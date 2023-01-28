eebedae9e684c6297629abf2bac3e6a1f15b957d
mustafa <mustafa.bhm3@gmail.com>
2023-01-27 19:55:06 -0700

    styles for the drawing

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export const HangmanDrawing = () => {
-  return <div>HangmanDrawing</div>;
+  return (
+    <div className="drawing">
+      <div className="right-leg"></div>
+      <div className="left-leg"></div>
+      <div className="right-arm"></div>
+      <div className="left-arm"></div>
+      <div className="torso"></div>
+      <div className="head"></div>
+      <div className="bar-down"></div>
+      <div className="horizontal-bar"></div>
+      <div className="bar"></div>
+      <div className="base"></div>
+    </div>
+  );
 };
