634b28ca596954f0efcbd266b8263464c40ce04c
mustafa <mustafa.bhm3@gmail.com>
2023-01-27 20:04:14 -0700

    create variable for body parts

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 
+const HEAD = <div className="head"></div>;
+const BODY = <div className="torso"></div>;
+const RIGHT_ARM = <div className="right-arm"></div>;
+const LEFT_ARM = <div className="left-arm"></div>;
+const RIGHT_LEG = <div className="right-leg"></div>;
+const LEFT_LEG = <div className="left-leg"></div>;
 export const HangmanDrawing = () => {
   return (
     <div className="drawing">
-      <div className="right-leg"></div>
-      <div className="left-leg"></div>
-      <div className="right-arm"></div>
-      <div className="left-arm"></div>
-      <div className="torso"></div>
-      <div className="head"></div>
+      {RIGHT_ARM}
+      {LEFT_ARM}
+      {RIGHT_LEG}
+      {LEFT_LEG}
+      {BODY}
+      {HEAD}
       <div className="bar-down"></div>
       <div className="horizontal-bar"></div>
       <div className="bar"></div>
