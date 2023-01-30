ac70a490b2440a252ddddb17e10b0e3202bfd308
mustafa <mustafa.bhm3@gmail.com>
2023-01-30 15:49:03 -0700

    display the keyboard

--- src/keyboard.tsx
+++ src/keyboard.tsx
@@ -1,5 +1,44 @@
 import React from "react";
 
 export const Keyboard = () => {
-  return <div className="keyboard"></div>;
+  const keyboard = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  return (
+    <div className="keyboard">
+      {keyboard.map((letter) => {
+        return (
+          <button key={letter} className="btn single-letter">
+            {letter}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
